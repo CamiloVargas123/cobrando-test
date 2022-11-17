9ec54f61fefe5977589864693fdc956beeb268b5
Camilo Vargas <camilovargasingm@gmail.com>
2022-11-16 22:03:08 -0500

    add import missings

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import { Provider } from 'react-redux'
+import store from './redux/store'
+import './config/axios.config'
+import { ThemeProvider } from '@mui/material'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5// milisegundo * segundo * minutos
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <Provider store={store}>
+            <App />
+        </Provider>
+      </ChakraProvider>
+
+    </QueryClientProvider>
   </React.StrictMode>
 )
